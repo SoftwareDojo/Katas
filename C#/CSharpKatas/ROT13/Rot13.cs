b789f7e2767fefc70a622e1843f1968a5cbc9734
SoftwareDojo <nextexception@gmail.com>
2016-04-11 08:19:12 +0200

    Final fixes and refactroings for the ROT13 Kata

--- C#/CSharpKatas/ROT13/Rot13.cs
+++ C#/CSharpKatas/ROT13/Rot13.cs
@@ -4,64 +4,69 @@ namespace CSharpKatas.ROT13
 {
     public class Rot13
     {
-        private readonly Dictionary<int, int> m_EncodeTable; 
+        private const int s_DefaultOffset = 13;
+        private readonly IDictionary<int, int> m_EncodeTable;
+        private readonly IDictionary<int, int> m_DecodeTable; 
 
-        public Rot13()
+        public Rot13() : this(s_DefaultOffset) { }
+
+        public Rot13(int offset)
         {
             m_EncodeTable = new Dictionary<int, int>();
+            m_DecodeTable = new Dictionary<int, int>();
 
-            // numbers 
-            //for (int i = 48; i < 58; i++)
-            //{
-            //    m_EncodeTable.Add(i, i+13);
-            //}
+            AddToTable('a', 'z', offset);
+            AddToTable('A', 'Z', offset);    
+        }
 
-            // abc
-            for (int i = 97; i < 123; i++)
+        private void AddToTable(int start, int end, int offset)
+        {
+            for (int i = start; i <= end; i++)
             {
-                int index = i + 13;
-                if (i > 108) index = i - 13;
+                int index = i + offset;
+                if (i > end - offset)
+                {
+                    index = i - (end - start) + offset -1;
+                }
 
                 m_EncodeTable.Add(i, index);
+                m_DecodeTable.Add(index, i);
             }
         }
 
         public string Encode(string text)
         {
-            text = TextNormalize(text.ToLower());
-            var chars = text.ToCharArray();
-
-            for (int i = 0; i < chars.Length; i++)
-            {
-                if (m_EncodeTable.ContainsKey(chars[i]))
-                {
-                    chars[i] = (char)m_EncodeTable[chars[i]];
-                }
-            }
-
-            return new string(chars);
+            return Translate(NormalizeText(text), m_EncodeTable);
         }
 
         public string Decode(string text)
         {
-            var chars = text.ToLower().ToCharArray();
+            return Translate(text, m_DecodeTable);
+        }
+
+        private string Translate(string text, IDictionary<int, int> table)
+        {
+            var chars = text.ToCharArray();
 
             for (int i = 0; i < chars.Length; i++)
             {
-                if (m_EncodeTable.ContainsKey(chars[i]))
+                if (table.ContainsKey(chars[i]))
                 {
-                    chars[i] = (char)m_EncodeTable[chars[i]];
+                    chars[i] = (char)table[chars[i]];
                 }
             }
 
             return new string(chars);
         }
 
-        private string TextNormalize(string text)
+        private string NormalizeText(string text)
         {
             text = text.Replace("ä", "ae");
+            text = text.Replace("Ä", "Ae");
             text = text.Replace("ö", "oe");
+            text = text.Replace("Ö", "Oe");
             text = text.Replace("ü", "ue");
+            text = text.Replace("Ü", "Ue");
             text = text.Replace("ß", "ss");
 
             return text;
