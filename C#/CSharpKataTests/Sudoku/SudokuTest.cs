7156593274854e3847b243a338abc5d4a0e29839
SoftwareDojo <nextexception@gmail.com>
2016-04-15 08:05:28 +0200

    Removed unused code in Sudoku Solver Kata

--- C#/CSharpKataTests/Sudoku/SudokuTest.cs
+++ C#/CSharpKataTests/Sudoku/SudokuTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CSharpKatas.Sudoku;
 using NUnit.Framework;
 
@@ -63,86 +62,28 @@ namespace CSharpKataTests.Sudoku
     public class SudokuTest
     {
         [Test]
-        public void Test()
+        public void Test_Solve_9x_Sudoku()
         {
-            var result = new SudokuSolver().Solve(
-                new string[] {
-                    "3,9,4,0,0,2,6,7,0" ,
-                    "0,0,0,3,0,0,4,0,0" ,
-                    "5,0,0,6,9,0,0,2,0" ,
 
-                    "0,4,5,0,0,0,9,0,0" ,
-                    "6,0,0,0,0,0,0,0,7" ,
-                    "0,0,7,0,0,0,5,8,0" ,
-
-                    "0,1,0,0,6,7,0,0,8" ,
-                    "0,0,9,0,0,8,0,0,0" ,
-                    "0,2,6,4,0,0,7,3,5"});
-
-            Assert.AreEqual("3,9,4,8,5,2,6,7,1", result[0]);
-            Assert.AreEqual("2,6,8,3,7,1,4,5,9", result[1]);
-            Assert.AreEqual("5,7,1,6,9,4,8,2,3", result[2]);
-            Assert.AreEqual("1,4,5,7,8,3,9,6,2", result[3]);
-            Assert.AreEqual("6,8,2,9,4,5,3,1,7", result[4]);
-            Assert.AreEqual("9,3,7,1,2,6,5,8,4", result[5]);
-            Assert.AreEqual("4,1,3,5,6,7,2,9,8", result[6]);
-            Assert.AreEqual("7,5,9,2,3,8,1,4,6", result[7]);
-            Assert.AreEqual("8,2,6,4,1,9,7,3,5", result[8]);
         }
 
         [Test]
-        public void Test_4xSquare_GetAllValues()
+        public void Test_Solve_4x_Sudoku()
         {
-            // arrange
-            var square = new Square(new int[2, 2] { { 0, 3 }, { 4, 0 } });
+            // actual       // expected
+            //[  3|4  |     [2 3|4 1|
+            //|4  |  3|     |4 1|2 3|
+            //|1  |  2|     |1 4|3 2|
+            //|  2|1  |     |3 2|1 4|
 
-            // act and assert
-            CollectionAssert.AreEqual(new List<int> { 3, 4 }, square.GetValues());
-        }
-
-        [Test]
-        public void Test_9xSquare_GetAllValues()
-        {
-            // arrange
-            var square = new Square(new int[3, 3] { { 3, 9, 4 }, { 0, 0, 0 }, { 5, 0, 0 } });
-
-            // act and assert
-            CollectionAssert.AreEqual(new List<int> { 3, 9, 4, 5 }, square.GetValues());
-        }   
-
-        [Test]
-        public void Test_Square_GetRowValues()
-        {
-            // arrange
-            var square = new Square(new int[2, 2] { { 0, 3 }, { 4, 0 } });
-
-            // act and assert
-            CollectionAssert.AreEqual(new List<int> { 3 }, square.GetRowValues(0));
-            CollectionAssert.AreEqual(new List<int> { 4 }, square.GetRowValues(1));
-        }
-
-        [Test]
-        public void Test_Square_GetColumnValues()
-        {
-            // arrange
-            var square = new Square(new int[2, 2] { { 0, 3 }, { 4, 0 } });
-
-            // act and assert
-            CollectionAssert.AreEqual(new List<int> { 4 }, square.GetColumnValues(0));
-            CollectionAssert.AreEqual(new List<int> { 3 }, square.GetColumnValues(1));
-        }
-
-        [Test]
-        public void TestSqrt()
-        {
             var solver = new SudokuSolver();
-            solver.Load(new List<int[,]>
+            solver.Solve(new List<int[,]>
             {
                 new [,] { { 0, 3 }, { 4, 0 } },
-                new [,] { { 4, 0 }, { 0, 2 } },
+                new [,] { { 4, 0 }, { 0, 3 } },
                 new [,] { { 1, 0 }, { 0, 2 } },
                 new [,] { { 0, 2 }, { 1, 0 } }
-            }) ;
+            });
         }
     }
 }
--- C#/CSharpKatas/Sudoku/Sudoku.cs
+++ C#/CSharpKatas/Sudoku/Sudoku.cs
@@ -1,36 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CSharpKatas.Sudoku
 {
     public class SudokuSolver
     {
-        private readonly int m_Size;
-        private const char s_Seperator = ',';
-
         private Square[] m_Squares;
 
-        public SudokuSolver() : this(9) { }
-
-        private SudokuSolver(int size)
+        public void Solve(IList<int[,]> squaresValues)
         {
-            m_Size = size;
-        }
+            Load(squaresValues);
 
-        public void Load(IList<int[,]> squaresValues)
-        {
-            m_Squares = new Square[squaresValues.Count];
-
-            for (int i = 0; i < squaresValues.Count; i++)
-            {
-                m_Squares[i] = new Square(squaresValues[i]);
-            }
-        }
-
-        public void Start()
-        {
-            double squareRoot = Math.Sqrt(m_Squares.Length);
+            int squareSize = (int)Math.Sqrt(m_Squares.Length);
             bool changed;
 
             do
@@ -39,126 +20,34 @@ namespace CSharpKatas.Sudoku
 
                 foreach (Square square in m_Squares)
                 {
-                    var values = square.GetValues();
-                    //if (values.)
+                    bool squareChanged = SolveSquare(square, squareSize);
+                    if (!changed && squareChanged) changed = true;
                 }
 
             } while (changed);
 
         }
 
-        public string[] Solve(string[] rows)
+        private bool SolveSquare(Square square, int squareSize)
         {
-            var cells = PrepareCells(rows);
-            bool changed;
-
-            do
+            for (int i = 0; i < squareSize; i++)
             {
-                changed = false;
-
-                for (int row = 0; row < m_Size; row++)
-                {
-                    for (int column = 0; column < m_Size; column++)
-                    {
-                        if (cells[row, column] > 0 ) continue;
-
-                        int value;
-                        if (TryGetCellValue(cells, row, column, out value))
-                        {
-                            cells[row, column] = value;
-                            changed = true;
-                        }
-                    }
-                }
-
-            } while (changed);
-            // in block
-            // in row
-            // in column
-
-            return PrepareStrings(cells);
-        }
-
-        private bool TryGetCellValue(int[,] cells, int row, int column, out int value)
-        {
-            value = 0;
-            var usedValues = GetCellValues();
-
-            // row 
-            for (int i = 0; i < m_Size; i++)
-            {
-                var cellValue = cells[row, i];
-                if (cellValue == 0 || usedValues[cellValue]) continue;
-                usedValues[cellValue] = true;
+                var columnValues = square.GetColumnValues(i);
             }
-
-            // column
-            for (int i = 0; i < m_Size; i++)
-            {
-                var cellValue = cells[i, column];
-                if (cellValue == 0 || usedValues[cellValue]) continue;
-                usedValues[cellValue] = true;
-            }
-
-
-
-            var possibleValues = usedValues.Where(pair => !pair.Value).Select(pair => pair.Key).ToList();
-            if (possibleValues.Count == 1)
-            {
-                value = possibleValues[0];
-                return true;
-            } 
 
             return false;
         }
 
-        private IDictionary<int, bool> GetCellValues()
+
+
+        private void Load(IList<int[,]> squaresValues)
         {
-            var result = new Dictionary<int, bool>();
+            m_Squares = new Square[squaresValues.Count];
 
-            for (int i = 1; i <= m_Size; i++)
+            for (int i = 0; i < squaresValues.Count; i++)
             {
-                result.Add(i, false);
+                m_Squares[i] = new Square(squaresValues[i]);
             }
-
-            return result;
-        } 
-
-        private int[,] PrepareCells(string[] rows)
-        {
-            if (rows.Length != m_Size) throw new ArgumentException(string.Format("expected {0} rows", m_Size));
-            int[,] cells = new int[m_Size,m_Size];
-
-            for (int r = 0; r < m_Size; r++)
-            {
-                var columns = rows[r].Split(s_Seperator).Select(column => Convert.ToInt32(column)).ToArray();
-                if (columns.Length != m_Size) throw new ArgumentException(string.Format("expected {0} columns in row {1}", m_Size, r+1));
-
-                for (int c = 0; c < columns.Length; c++)
-                {
-                    cells[r, c] = columns[c];
-                }
-            }
-
-            return cells;
-        }
-
-        private string[] PrepareStrings(int[,] cells)
-        {
-            var result = new string[m_Size];
-
-            for (int i = 0; i < m_Size; i++)
-            {
-                var list = new List<int>();
-                for (int j = 0; j < m_Size; j++)
-                {
-                    list.Add(cells[i,j]);
-                }
-
-                result[i] = string.Join(s_Seperator.ToString(), list);
-            }
-
-            return result;
         }
     }
 }
