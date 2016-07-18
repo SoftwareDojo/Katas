6597dc67fe0b3b394571c9d3f464095ae435fb55
SoftwareDojo <nextexception@gmail.com>
2016-07-18 09:12:18 +0200

    Updated solution for SudokuSolver

--- C#/CSharpKataTests/Sudoku/SudokuTest.cs
+++ C#/CSharpKataTests/Sudoku/SudokuTest.cs
@@ -1,89 +1,181 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using CSharpKatas.Sudoku;
 using NUnit.Framework;
 
 namespace CSharpKataTests.Sudoku
 {
-    //PUZZLE
-    //[  ,  , 3,  ,  ,  ,  ,  ,  ]
-    //[ 4,  ,  ,  , 8,  ,  , 3, 6]
-    //[  ,  , 8,  ,  ,  , 1,  , .]
-    //[  , 4,  ,  , 6,  ,  , 7, 3]
-    //[  ,  ,  , 9,  ,  ,  ,  , .]
-    //[  ,  ,  ,  ,  , 2,  ,  , 5]
-    //[  ,  , 4,  , 7,  ,  , 6, 8]
-    //[ 6,  ,  ,  ,  ,  ,  ,  , .]
-    //[ 7,  ,  , 6,  ,  , 5,  , .]
-
-    //SOLUTION
-    //[ 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    //[ 4, 5, 7, 1, 8, 9, 2, 3, 6]
-    //[ 9, 6, 8, 3, 2, 7, 1, 5, 4]
-    //[ 2, 4, 9, 5, 6, 1, 8, 7, 3]
-    //[ 5, 7, 6, 9, 3, 8, 4, 1, 2]
-    //[ 8, 3, 1, 7, 4, 2, 6, 9, 5]
-    //[ 3, 1, 4, 2, 7, 5, 9, 6, 8]
-    //[ 6, 9, 5, 8, 1, 4, 3, 2, 7]
-    //[ 7, 8, 2, 6, 9, 3, 5, 4, 1]
-
-    //PUZZLE
-    //[3 9 4|    2|6 7  ]
-    //|     |3    |4    |
-    //|5    |6 9  |  2  |
-    //|  4 5|     |9    |
-    //|6    |     |    7|
-    //|    7|     |5 8  |
-    //|  1  |  6 7|    8|
-    //|    9|    8|     |
-    //|  2 6|4    |7 3 5|
-
-    //SOLUTION
-    //|3 9 4|8 5 2|6 7 1|
-    //|2 6 8|3 7 1|4 5 9|
-    //|5 7 1|6 9 4|8 2 3|
-    //|1 4 5|7 8 3|9 6 2|
-    //|6 8 2|9 4 5|3 1 7|
-    //|9 3 7|1 2 6|5 8 4|
-    //|4 1 3|5 6 7|2 9 8|
-    //|7 5 9|2 3 8|1 4 6|
-    //|8 2 6|4 1 9|7 3 5|
-
-    //[ 8, 5,  ,  ,  , 2, 4,  ,  ]
-    //[ 7, 2,  ,  ,  ,  ,  ,  , 9]
-    //[  ,  , 4,  ,  ,  ,  ,  ,  ]
-    //[  ,  ,  , 1,  , 7,  ,  , 2]
-    //[ 3,  , 5,  ,  ,  , 9,  ,  ]
-    //[  , 4,  ,  ,  ,  ,  ,  ,  ]
-    //[  ,  ,  ,  , 8,  ,  , 7,  ]
-    //[  , 1, 7,  ,  ,  ,  ,  ,  ]
-    //[  ,  ,  ,  , 3, 6,  , 4,  ]
-
     [TestFixture]
     public class SudokuTest
     {
         [Test]
-        public void Test_Solve_9x_Sudoku()
+        public void Unsolvable()
         {
+            // arrange
+            var solver = new SudokuSolver();
+            var actual = GetEmpty4Sudoku();
 
+            // act
+            solver.Solve(actual);
+
+            // assert
+            var expected = GetEmpty4Sudoku();
+            Assert.IsTrue(expected.Cast<int>().SequenceEqual(actual.Cast<int>()));
         }
 
         [Test]
-        public void Test_Solve_4x_Sudoku()
+        public void SolveByColumn()
         {
-            // actual       // expected
-            //[  3|4  |     [2 3|4 1|
-            //|4  |  3|     |4 1|2 3|
-            //|1  |  2|     |1 4|3 2|
-            //|  2|1  |     |3 2|1 4|
-
+            // arrange
             var solver = new SudokuSolver();
-            solver.Solve(new List<int[,]>
+            var actual = GetSolved4Sudoku();
+            actual[0, 0] = 0;
+            actual[0, 1] = 0;
+            actual[0, 2] = 0;
+            actual[0, 3] = 0;
+            //|0 0|0 0|
+            //|4 1|2 3|
+            //|1 4|3 2|
+            //|3 2|1 4|
+
+            // act
+            solver.Solve(actual);
+
+            // assert
+            var expected = GetSolved4Sudoku();
+            Assert.IsTrue(expected.Cast<int>().SequenceEqual(actual.Cast<int>()));
+        }
+
+        [Test]
+        public void SolveByRow()
+        {
+            // arrange
+            var solver = new SudokuSolver();
+            var actual = GetSolved4Sudoku();
+            actual[0, 3] = 0;
+            actual[1, 3] = 0;
+            actual[2, 3] = 0;
+            actual[3, 3] = 0;
+            //|2 3|4 0|
+            //|4 1|2 0|
+            //|1 4|3 0|
+            //|3 2|1 0|
+
+            // act
+            solver.Solve(actual);
+
+            // assert
+            var expected = GetSolved4Sudoku();
+            Assert.IsTrue(expected.Cast<int>().SequenceEqual(actual.Cast<int>()));
+        }
+
+        [Test]
+        public void Solve_9_Sudoku()
+        {
+            // arrange
+            var solver = new SudokuSolver();
+            var actual = GetUnSolved9Sudoku();
+
+            // act
+            solver.Solve(actual);
+
+            // assert
+            var expected = GetSolved9Sudoku();
+            Assert.IsTrue(expected.Cast<int>().SequenceEqual(actual.Cast<int>()));
+        }
+
+        [Test]
+        public void Solve_4_Sudoku()
+        {
+            // arrange
+            var solver = new SudokuSolver();
+            var actual = GetSolved4Sudoku();
+            actual[0, 0] = 0;
+            actual[0, 3] = 0;
+            actual[1, 1] = 0;
+            actual[1, 2] = 0;
+            actual[2, 1] = 0;
+            actual[2, 2] = 0;
+            actual[3, 0] = 0;
+            actual[3, 3] = 0;
+            //|  3|4  |
+            //|4  |  3|
+            //|1  |  2|
+            //|  2|1  |
+
+            // act
+            solver.Solve(actual);
+
+            // assert
+            var expected = GetSolved4Sudoku();
+            Assert.IsTrue(expected.Cast<int>().SequenceEqual(actual.Cast<int>()));
+        }
+
+        private int[,] GetEmpty4Sudoku()
+        {
+            return new[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            //|0 0|0 0|
+            //|0 0|0 0|
+            //|0 0|0 0|
+            //|0 0|0 0|
+        }
+
+        private int[,] GetSolved4Sudoku()
+        {
+            return new[,] { { 2, 3, 4, 1 }, { 4, 1, 2, 3 }, { 1, 4, 3, 2 }, { 3, 2, 1, 4 } };
+            //|2 3|4 1|
+            //|4 1|2 3|
+            //|1 4|3 2|
+            //|3 2|1 4|
+        }
+
+        private int[,] GetSolved9Sudoku()
+        {
+            return new[,]
             {
-                new [,] { { 0, 3 }, { 4, 0 } },
-                new [,] { { 4, 0 }, { 0, 3 } },
-                new [,] { { 1, 0 }, { 0, 2 } },
-                new [,] { { 0, 2 }, { 1, 0 } }
-            });
+                {3, 9, 4, 8, 5, 2, 6, 7, 1},
+                {2, 6, 8, 3, 7, 1, 4, 5, 9},
+                {5, 7, 1, 6, 9, 4, 8, 2, 3},
+                {1, 4, 5, 7, 8, 3, 9, 6, 2},
+                {6, 8, 2, 9, 4, 5, 3, 1, 7},
+                {9, 3, 7, 1, 2, 6, 5, 8, 4},
+                {4, 1, 3, 5, 6, 7, 2, 9, 8},
+                {7, 5, 9, 2, 3, 8, 1, 4, 6},
+                {8, 2, 6, 4, 1, 9, 7, 3, 5},
+            };
+            //|3 9 4|8 5 2|6 7 1|
+            //|2 6 8|3 7 1|4 5 9|
+            //|5 7 1|6 9 4|8 2 3|
+            //|1 4 5|7 8 3|9 6 2|
+            //|6 8 2|9 4 5|3 1 7|
+            //|9 3 7|1 2 6|5 8 4|
+            //|4 1 3|5 6 7|2 9 8|
+            //|7 5 9|2 3 8|1 4 6|
+            //|8 2 6|4 1 9|7 3 5|
+        }
+
+        private int[,] GetUnSolved9Sudoku()
+        {
+            return new[,]
+            {
+                {3, 9, 4, 0, 0, 2, 6, 7, 0},
+                {0, 0, 0, 3, 0, 0, 4, 0, 0},
+                {5, 0, 0, 6, 9, 0, 0, 2, 0},
+                {0, 4, 5, 0, 0, 0, 9, 0, 0},
+                {6, 0, 0, 0, 0, 0, 0, 0, 7},
+                {0, 0, 7, 0, 0, 0, 5, 8, 0},
+                {0, 1, 0, 0, 6, 7, 0, 0, 8},
+                {0, 0, 9, 0, 0, 8, 0, 0, 0},
+                {0, 2, 6, 4, 0, 0, 7, 3, 5},
+            };
+            //[3 9 4|    2|6 7  ]
+            //|     |3    |4    |
+            //|5    |6 9  |  2  |
+            //|  4 5|     |9    |
+            //|6    |     |    7|
+            //|    7|     |5 8  |
+            //|  1  |  6 7|    8|
+            //|    9|    8|     |
+            //|  2 6|4    |7 3 5|
         }
     }
 }
--- C#/CSharpKatas/Sudoku/Sudoku.cs
+++ C#/CSharpKatas/Sudoku/Sudoku.cs
@@ -5,49 +5,117 @@ namespace CSharpKatas.Sudoku
 {
     public class SudokuSolver
     {
-        private Square[] m_Squares;
-
-        public void Solve(IList<int[,]> squaresValues)
+        private struct Cell
         {
-            Load(squaresValues);
+            public int X { get; }
+            public int Y { get; }
 
-            int squareSize = (int)Math.Sqrt(m_Squares.Length);
-            bool changed;
-
-            do
+            public Cell(int x, int y)
             {
-                changed = false;
+                X = x;
+                Y = y;
+            }
+        }
 
-                foreach (Square square in m_Squares)
+        public void Solve(int[,] cells)
+        {
+            int xLength = cells.GetLength(0);
+            int yLength = cells.GetLength(1);
+            int sqrtRoot = (int)Math.Sqrt(xLength);
+
+            var emptyCells = GetEmptyCells(cells, xLength, yLength);
+            bool unsolvable = false;
+
+            while (!unsolvable)
+            {
+                unsolvable = true;
+
+                foreach (var cell in emptyCells)
                 {
-                    bool squareChanged = SolveSquare(square, squareSize);
-                    if (!changed && squareChanged) changed = true;
+                    var rowValues = GetRowValues(cells, cell.X, yLength);
+                    var columnValues = GetColumnValues(cells, cell.Y, xLength);
+                    var squareValues = GetSquareValues(cells, cell.X, cell.Y, sqrtRoot);
+
+                    var values = GetPossibleValues(rowValues, columnValues, squareValues, xLength);
+                    
+                    if (values.Count != 1) continue;
+                    {
+                        cells[cell.X, cell.Y] = values[0];
+                        unsolvable = false;
+                    }
                 }
-
-            } while (changed);
-
+            }
         }
 
-        private bool SolveSquare(Square square, int squareSize)
+        private IList<Cell> GetEmptyCells(int[,] cells, int xLength, int yLength)
         {
-            for (int i = 0; i < squareSize; i++)
+            var result = new List<Cell>();
+
+            for (int x = 0; x < xLength; x++)
             {
-                var columnValues = square.GetColumnValues(i);
+                for (int y = 0; y < yLength; y++)
+                {
+                    if (cells[x, y] != 0) continue;
+                    result.Add(new Cell(x, y));
+                }
             }
 
-            return false;
+            return result;
         }
 
-
-
-        private void Load(IList<int[,]> squaresValues)
+        private IList<int> GetRowValues(int[,] cells, int x, int yLength)
         {
-            m_Squares = new Square[squaresValues.Count];
+            var result = new List<int>();
 
-            for (int i = 0; i < squaresValues.Count; i++)
+            for (int y = 0; y < yLength; y++)
             {
-                m_Squares[i] = new Square(squaresValues[i]);
+                if (cells[x, y] != 0) result.Add(cells[x, y]);
             }
+
+            return result;
+        }
+
+        private IList<int> GetColumnValues(int[,] cells, int y, int xLength)
+        {
+            var result = new List<int>();
+
+            for (int x = 0; x < xLength; x++)
+            {
+                if (cells[x, y] != 0) result.Add(cells[x, y]);
+            }
+
+            return result;
+        }
+
+        private IList<int> GetSquareValues(int[,] cells, int x, int y, int sqrtRoot)
+        {
+            var result = new List<int>();
+
+            int xLeft = x % sqrtRoot;
+            int yLeft = y % sqrtRoot;
+
+            for (var i = x - xLeft; i < x - xLeft + sqrtRoot; i++)
+            {
+                for (var j = y - yLeft; j < y - yLeft + sqrtRoot; j++)
+                {
+                    if (cells[i, j] != 0) result.Add(cells[i, j]);
+                }
+            }
+
+            return result;
+        }
+
+        private IList<int> GetPossibleValues(IList<int> xValues, IList<int> yValues, IList<int> sValues, int range)
+        {
+            var result = new List<int>();
+
+            for (int i = 1; i <= range; i++)
+            {
+                if (xValues.Contains(i) || yValues.Contains(i) || sValues.Contains(i)) continue;
+                result.Add(i);
+            }
+
+            return result;
         }
     }
 }
