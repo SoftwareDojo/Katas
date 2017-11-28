479feb1408396ede7a518e40d5f05a7deeb1aadc
SoftwareDojo <NextException@gmail.com>
2017-11-28 16:00:04 +0100

    New F# kata sampels!

---
+++ F#/Katas.Tests/FizzBuzz/FizzBuzzTests.fs
@@ -0,0 +1,19 @@
+﻿module Tests
+
+open System
+open Xunit
+
+open Katas
+
+[<Theory>]
+[<InlineData(0, "")>]
+[<InlineData(1, "1")>]
+[<InlineData(2, "2")>]
+[<InlineData(3, "fizz")>]
+[<InlineData(4, "4")>]
+[<InlineData(5, "buzz")>]
+[<InlineData(6, "fizz")>]
+[<InlineData(10, "buzz")>]
+[<InlineData(15, "fizzbuzz")>]
+let ``FizzBuzz2`` (value : int, expected : string) =
+    Assert.Equal(expected, FizzBuzz.fizzbuzz value)
---
+++ F#/Katas/FizzBuzz/FizzBuzz.fs
@@ -0,0 +1,13 @@
+﻿namespace Katas
+
+module FizzBuzz =
+
+    let fizzbuzz n = 
+        if n <= 0 then ""
+        else
+            match (n % 3) = 0, (n % 5) = 0 with
+                | true,false -> "fizz"
+                | false,true -> "buzz"
+                | true,true -> "fizzbuzz"
+                | false,false -> n.ToString()
+            
